603355917d7b1e7d15c332e78f0d2c22e481d9c8
Rahul Rao <RAOR378647@exactsoftware.com>
2020-12-13 19:38:06 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { from, fromEvent, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { KeyValuePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,93 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-observable-pipe-map';
+
+  constructor(private http: HttpClient,
+    private keyValue: KeyValuePipe) {
+  }
+
+  srcArray = from([1, 2, 3, 4]);
+  srcName$ = from(['John', 'Tom', 'Katy'])
+  srcObject = from([
+    { fName: 'Sachin', lName: "Tendulkar" },
+    { fName: 'Rahul', lName: "Dravid" },
+    { fName: 'Saurav', lName: "Ganguly" },
+  ]);
+  @ViewChild('btn1', { static: true }) button1: ElementRef;
+
+  ngOnInit() {
+    this.multiplyBy2();
+    console.log('-----------------------');
+    this.toUpperCase();
+    console.log('-----------------------');
+    this.mapToSingleProperty();
+    console.log('-----------------------');
+    this.mulitpleMaps();
+    console.log('-----------------------');
+  }
+
+  multiplyBy2() {
+    this.srcArray
+      .pipe(map((val, i) => {         //index
+        console.log('index- ', i)                //0
+        return val * 2;
+      }))
+      .subscribe(val => { console.log(val) })
+  }
+
+  toUpperCase() {
+    this.srcName$
+      .pipe(map(data => {
+        return data.toUpperCase();
+      }))
+      .subscribe(data => console.log(data))
+  }
+
+  mapToSingleProperty() {
+    this.srcObject
+      .pipe(map(data => { return data.fName + ' ' + data.lName }))
+      .subscribe(data => { console.log(data) })
+  }
+
+  mulitpleMaps() {
+    this.srcArray
+    .pipe(
+      map(val => {
+        return val + 10;
+      }),
+      map((val, i) => {
+        return val * 2;
+      }))
+    .subscribe(val => { console.log(val) })
+  }
+
+  //Form event
+  ngAfterViewInit() {
+    this.buttonClick();
+  }
+
+  buttonClick() {
+    fromEvent(this.button1.nativeElement, 'click')
+      .pipe(map(ev => (ev as any).clientX))
+      .subscribe(res => console.log(res));
+    console.log('-----------------------');
+  }
+
+  //http
+  $dogsBreed(): Observable<any> {
+    return this.http.get<any>("https://dog.ceo/api/breeds/list/all")
+  }
+
+  getDogsBreed() {
+
+    this.$dogsBreed()
+      .pipe(map(data => {
+        var dogs = this.keyValue.transform(data.message)
+        console.log(dogs)
+        console.log('-----------------------');
+      }))
+      .subscribe();
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { KeyValuePipe } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [KeyValuePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
